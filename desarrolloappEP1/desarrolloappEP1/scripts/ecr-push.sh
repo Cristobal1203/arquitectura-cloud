9ed25feb264d101ec4ef8a68777242debe2e9040
Cristobal1203 <cristo.tapiap@duocuc.cl>
2026-09-10 02:10:47 -0300

    c

--- desarrolloappEP1/desarrolloappEP1/scripts/ecr-push.sh
+++ desarrolloappEP1/desarrolloappEP1/scripts/ecr-push.sh
@@ -6,16 +6,16 @@ echo "=== Account: $ACCOUNT_ID | Region: $REGION ==="
 
 aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com
 
-REPOS=("freshbox-frontend" "freshbox-get-products" "freshbox-create-product" "freshbox-update-product" "freshbox-delete-product")
+REPOS=("freshbox/frontend" "freshbox/get-products" "freshbox/create-product" "freshbox/update-product" "freshbox/delete-product")
 for REPO in "${REPOS[@]}"; do
     aws ecr create-repository --repository-name $REPO --region $REGION 2>/dev/null || echo "$REPO ya existe"
 done
 
-docker build --platform linux/arm64 -t freshbox-frontend ./microservicioFrontend
-docker build --platform linux/arm64 -t freshbox-get-products ./microserviciosBackend/get-products
-docker build --platform linux/arm64 -t freshbox-create-product ./microserviciosBackend/create-product
-docker build --platform linux/arm64 -t freshbox-update-product ./microserviciosBackend/update-product
-docker build --platform linux/arm64 -t freshbox-delete-product ./microserviciosBackend/delete-product
+docker build --platform linux/arm64 -t freshbox/frontend ./microservicioFrontend
+docker build --platform linux/arm64 -t freshbox/get-products ./microserviciosBackend/get-products
+docker build --platform linux/arm64 -t freshbox/create-product ./microserviciosBackend/create-product
+docker build --platform linux/arm64 -t freshbox/update-product ./microserviciosBackend/update-product
+docker build --platform linux/arm64 -t freshbox/delete-product ./microserviciosBackend/delete-product
 
 for REPO in "${REPOS[@]}"; do
     docker tag $REPO $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$REPO:latest
